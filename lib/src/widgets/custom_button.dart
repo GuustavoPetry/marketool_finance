5fceaf5ad1d842b435438ebd39c4c785a60d456b
oGustavoPetry <ogustavopetry@gmail.com>
2025-06-22 06:37:56 -0300

    Padronizando botão + nova rota "/patrimony" (#29)
    * Adicionando gráfico na rota '/patrimony'
    
    * padronizando botão

--- lib/src/widgets/custom_button.dart
+++ lib/src/widgets/custom_button.dart
@@ -1,43 +1,39 @@
 import 'package:flutter/material.dart';
 
 class CustomButton extends StatelessWidget {
-  final String text;
+  final String label;
   final VoidCallback? onPressed;
-  final Widget child;
+  final Widget icon;
 
   const CustomButton({
     super.key,
-    required this.text,
+    required this.label,
     required this.onPressed,
-    this.child = const SizedBox.shrink(),
+    this.icon = const SizedBox.shrink(),
   });
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.all(5),
-      child: Center(
-        child: Container(
-          alignment: Alignment.center,
-          child: ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              backgroundColor: const Color(0xFF2E7D32),
-              foregroundColor: Colors.white,
-              disabledBackgroundColor: const Color(0xFF2E7D32),
-              disabledForegroundColor: Colors.white,
-              minimumSize: const Size(200, 48),
+      child: Container(
+        alignment: Alignment.center,
+        child: ElevatedButton.icon(
+          style: ElevatedButton.styleFrom(
+            textStyle: TextStyle(
+              fontSize: 18,
+              fontFamily: "RobotoMono",
+              fontWeight: FontWeight.bold,
             ),
-            onPressed: onPressed,
-            child: Text(
-              text,
-              style: const TextStyle(
-                fontSize: 20,
-                fontFamily: "RobotoMono",
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
-              ),
-            ),
+            backgroundColor: const Color(0xFF2E7D32),
+            foregroundColor: Colors.white,
+            disabledBackgroundColor: const Color(0xFF2E7D32),
+            disabledForegroundColor: Colors.white,
+            minimumSize: const Size(200, 48),
           ),
+          label: Text(label),
+          onPressed: onPressed,
+          icon: icon,
         ),
       ),
     );
