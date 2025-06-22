2742d7c21d500f948046ac01b089eecf47247b3f
GuustavoPetry <ogustavopetry@gmail.com>
2025-06-22 12:11:14 -0300

    Alterando cor de TextField

--- lib/src/widgets/custom_input_field.dart
+++ lib/src/widgets/custom_input_field.dart
@@ -36,11 +36,11 @@ class CustomInputField extends StatelessWidget {
             border: OutlineInputBorder(borderRadius: BorderRadius.circular(25)),
             enabledBorder: OutlineInputBorder(
               borderRadius: BorderRadius.circular(25),
-              borderSide: BorderSide(color: Colors.green, width: 1),
+              borderSide: BorderSide(color: const Color(0xFF2E7D32), width: 1),
             ),
             focusedBorder: OutlineInputBorder(
               borderRadius: BorderRadius.circular(25),
-              borderSide: BorderSide(color: Colors.green, width: 1),
+              borderSide: BorderSide(color: const Color(0xFF2E7D32), width: 1),
             ),
             fillColor: Color(0xFFE8F5E9),
             filled: true,
