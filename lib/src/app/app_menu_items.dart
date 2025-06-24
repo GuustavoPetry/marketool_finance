6e5b410f19d89dbabf3bab20e512e470a3900e5f
Alexandre Pinto <alexandre.pinto@proway.treina>
2025-06-24 09:05:58 -0300

    criando a tela central de ajuda

--- lib/src/app/app_menu_items.dart
+++ lib/src/app/app_menu_items.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:marketool_financer/src/models/menu_model.dart';
+import 'package:marketool_financer/src/views/help_center_view.dart';
 import 'package:marketool_financer/src/views/home_view.dart';
 import 'package:marketool_financer/src/views/patrimony_view.dart';
 import 'package:marketool_financer/src/views/upload_view.dart';
@@ -22,5 +23,11 @@ final List<MenuModel> appMenuItems = [
     icon: Icons.file_upload_sharp,
     route: "/upload",
     page: const UploadView(),
+  ),
+  MenuModel(
+    title: "Central de Ajuda",
+    icon: Icons.help_center,
+    route: "/help",
+    page: const HelpCenterView(),
   ),
 ];
