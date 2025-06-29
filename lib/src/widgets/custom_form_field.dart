6a1cd82949dc7c948025f62765e83420e3c1e81e
oGustavoPetry <ogustavopetry@gmail.com>
2025-06-29 12:48:52 -0300

    Customizando página de cadastro (#41)
    * Centralizando
    
    * Customizando página de cadastro

--- lib/src/widgets/custom_form_field.dart
+++ lib/src/widgets/custom_form_field.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class CustomFormField extends StatelessWidget {
+class CustomFormField extends StatefulWidget {
   final String text;
   final Icon icon;
   final TextInputType type;
@@ -19,36 +19,61 @@ class CustomFormField extends StatelessWidget {
   });
 
   @override
+  State<CustomFormField> createState() => _CustomFormFieldState();
+}
+
+class _CustomFormFieldState extends State<CustomFormField> {
+  final FocusNode _focusNode = FocusNode();
+  bool _isFocused = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _focusNode.addListener(() {
+      setState(() {
+        _isFocused = _focusNode.hasFocus;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _focusNode.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Center(
       child: SizedBox(
         width: 280,
         height: 50,
         child: TextFormField(
-          controller: inputController,
+          focusNode: _focusNode,
+          controller: widget.inputController,
           decoration: InputDecoration(
-            labelText: text,
+            labelText: widget.text,
             labelStyle: TextStyle(
               fontSize: 16,
               fontFamily: "RobotoMono",
               fontWeight: FontWeight.bold,
-              color: Color(0xFF2E7D32),
+              color: _isFocused ? Colors.green : const Color(0xFF2E5C4B),
             ),
-            prefixIcon: icon,
+            prefixIcon: widget.icon,
             border: OutlineInputBorder(borderRadius: BorderRadius.circular(25)),
             enabledBorder: OutlineInputBorder(
               borderRadius: BorderRadius.circular(25),
-              borderSide: BorderSide(color: const Color(0xFF2E7D32), width: 1),
+              borderSide: BorderSide(color: const Color(0xFF2E5C4B), width: 1),
             ),
             focusedBorder: OutlineInputBorder(
               borderRadius: BorderRadius.circular(25),
-              borderSide: BorderSide(color: const Color(0xFF2E7D32), width: 1),
+              borderSide: BorderSide(color: const Color(0xFF2E5C4B), width: 1),
             ),
             fillColor: Color(0xFFE8F5E9),
             filled: true,
           ),
-          obscureText: isObscure,
-          validator: inputValidator,
+          obscureText: widget.isObscure,
+          validator: widget.inputValidator,
         ),
       ),
     );
