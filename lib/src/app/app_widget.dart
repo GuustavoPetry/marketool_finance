dd4d832d1a69940b18f8b81039a98bf24d816d2e
oGustavoPetry <ogustavopetry@gmail.com>
2025-07-01 14:09:07 -0300

    Criando Página para recuperação de senha (#43)
    Co-authored-by: Alexandre Pinto <alexandrepinto199014@hotmail.com>

--- lib/src/app/app_widget.dart
+++ lib/src/app/app_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:marketool_financer/src/app/generate_routes.dart';
 import 'package:marketool_financer/src/services/auth_service.dart';
+import 'package:marketool_financer/src/views/forgot_password_view.dart';
 import 'package:marketool_financer/src/views/login_view.dart';
 import 'package:marketool_financer/src/views/register_view.dart';
 
@@ -15,6 +16,7 @@ class AppWidget extends StatelessWidget {
       routes: {
         "/login": (context) => LoginView(),
         "/register": (context) => RegisterView(),
+        "/forgot": (context) => ForgotPasswordView(),
         ...generateRoutes(),
       },
     );
