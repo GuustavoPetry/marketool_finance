2c5087c3b15ee5a04293035c7391682349707991
oGustavoPetry <ogustavopetry@gmail.com>
2025-08-13 13:24:30 -0300

    Renderizando dados de lucro na home table (#60)
    * Parametrizando tabela de dados
    
    * Renderizando dados de lucro na home table

--- lib/src/views/home_view.dart
+++ lib/src/views/home_view.dart
@@ -1,24 +1,84 @@
 import 'package:flutter/material.dart';
+import 'package:marketool_financer/src/services/auth_service.dart';
+import 'package:marketool_financer/src/services/custody_service.dart';
 import 'package:marketool_financer/src/widgets/custom_home_table.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
   const HomeView({super.key});
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  final _service = CustodyService();
+  late Future<List<Map<String, dynamic>>> _futureProfit;
+
+  @override
+  void initState() {
+    super.initState();
+    _futureProfit = _service.getUserProfit(AuthService.userId!);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: 15, vertical: 20),
-      child: ListView(
-        children: [
-          CustomHomeTable(
-            columnLabels: ["Ativo", "Δ Diária", "Δ Total"],
-            columnWidths: [120, 120, 120],
-            data: [
-              ["PETR4", 1.5, -0.8],
-              ["VALE3", -0.3, 2.1],
+      child: FutureBuilder<List<Map<String, dynamic>>>(
+        future: _futureProfit,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          if (snapshot.hasError) {
+            return Center(
+              child: Text(
+                "Erro: ${snapshot.error}",
+                style: TextStyle(color: Colors.white),
+              ),
+            );
+          }
+
+          if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return const Center(
+              child: Text(
+                "Nenhum dado encontrado",
+                style: TextStyle(color: Colors.white),
+              ),
+            );
+          }
+
+          final data = snapshot.data!;
+
+          final tableData = data
+              .map(
+                (item) => [
+                  item["assetTicker"],
+                  item["regularMarketChangePercent"],
+                  item["regularMarketChange"],
+                  item["profitPercentage"],
+                  item["profitValue"],
+                ],
+              )
+              .toList();
+
+          return ListView(
+            children: [
+              CustomHomeTable(
+                columnLabels: [
+                  "Ativo",
+                  "% Diária",
+                  "R\$ Diária",
+                  "% Total",
+                  "R\$ Total",
+                ],
+                columnWidths: [80, 120, 120, 120, 120],
+                data: tableData,
+              ),
             ],
-          ),
-        ],
+          );
+        },
       ),
     );
   }
